141a65b9573e00c1af38763ba3012efc4643c69d
Waylend <dustinbyers2@gmail.com>
2023-06-30 13:36:13 -0400

    added circle function

--- game2.js
+++ game2.js
@@ -9,8 +9,8 @@ window.onload = function(){
     //implement clicking functions
 
     //position of the player
-    let PlayerX = 0;
-    let PlayerY = 0;
+    let PlayerX = 100;
+    let PlayerY = 100;
     //direction of the player
     let PlayerDirX = 0;
     let PlayerDirY = 0;
@@ -27,16 +27,31 @@ window.onload = function(){
         ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
         ctx.beginPath();
 
-        drawRect(PlayerX,PlayerY,100,100,"",0,PlayerCol);
+        //drawRect(PlayerX,PlayerY,100,100,"",0,PlayerCol);
+        drawCircle(PlayerX,PlayerY,100,"",0,PlayerCol)
 
         window.requestAnimationFrame(draw);
 
     }
-
+    //xposition yposition width height border borderwidth fillcolor
     function drawRect(x,y,w,h,b,bw,c){
 
         ctx.rect(x,y,w,h);
         
+        if (c != ""){
+            ctx.fillStyle = c;
+            ctx.fill();
+        }
+        if (b != ""){
+            ctx.lineWidth = bw;
+            ctx.strokeStyle = b;
+            ctx.stroke();
+        }
+    }
+    //Xposition Yposition radius borderstyle borderWidth fillcolor
+    function drawCircle(x,y,r,b,bw,c){
+        ctx.arc(x,y,r,0,2 * Math.PI);
+
         if (c != ""){
             ctx.fillStyle = c;
             ctx.fill();
